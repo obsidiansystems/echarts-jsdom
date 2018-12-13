4843ec440f87714271ac190350dbe6097726c97a
Divam <dfordivam@gmail.com>
2018-12-14 05:37:23 +0900

    Add toJSVal instances and change cabal package name

--- src/ECharts.hs
+++ src/ECharts.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE FlexibleInstances #-}
 module ECharts
   ( initECharts
   , setOption
+  , setOptionJSVal
   , module X
   , ECharts
   )
@@ -11,11 +13,13 @@ import ECharts.ChartOptions as X
 import ECharts.Series as X
 import ECharts.Data as X
 import ECharts.Internal
+import ECharts.Internal.EChartSeries
 
 import Control.Monad (void)
 import qualified Data.Text as T
 import Language.Javascript.JSaddle
 import GHCJS.DOM.Types (Element)
+import Data.Some (Some)
 
 initECharts :: GHCJS.DOM.Types.Element -> JSM ECharts
 initECharts e = do
@@ -25,7 +29,17 @@ initECharts e = do
 
 setOption :: ECharts -> ChartOptions -> JSM ()
 setOption c opts = do
+  options <- toJSVal opts
+  setOptionJSVal c options
+
+setOptionJSVal :: ECharts -> JSVal -> JSM ()
+setOptionJSVal c options = do
   f <- eval $ T.pack "(function(e, opt) { e['setOption'](opt); })"
   let chart = unECharts c
-  options <- toJSVal =<< toEChartConfig opts
   void $ call f f [chart, options]
+
+instance ToJSVal ChartOptions where
+  toJSVal o = toJSVal =<< toEChartConfig o
+
+instance ToJSVal (Some SeriesT) where
+  toJSVal o = toJSVal =<< toEChartSeries o
